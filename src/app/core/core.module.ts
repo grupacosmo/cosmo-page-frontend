ac5ae077157dd47b96c67a3ce464ea8f1b8bc8cf
Mateusz Jamróz <jamr.mat@gmail.com>
2023-12-23 21:07:00 +0100

    fix

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { RouterLink } from '@angular/router';
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent
+  ],
+  exports: [
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterLink
+  ]
+})
+export class CoreModule { }
