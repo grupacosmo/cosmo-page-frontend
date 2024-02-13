2237c15196aaf31ddbbebe501fb7fad1365bad50
acygan <acygan51@gmail.com>
2024-02-13 21:14:24 +0100

    Header & style init

--- src/app/modules/home/pages/home/home.component.ts
+++ src/app/modules/home/pages/home/home.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  protected text = {
+    description: 'Studenckie koło naukowe Politechniki Krakowskiej',
+    joinUs: 'Dołącz do nas'
+  }
 
+  protected socialMedia = [
+    {
+      icon: '',
+      href: '',
+    },
+    {
+      icon: '',
+      href: ''
+    },
+    {
+      icon: '',
+      href: ''
+    }
+  ]
 }
