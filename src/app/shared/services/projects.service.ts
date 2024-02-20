3a06a5377b99b47df8c9c20fca021fda378e3961
tnt120 <apaj04@gmail.com>
2024-02-20 02:35:22 +0100

    [fix] changed project section styles & projects titles

--- src/app/shared/services/projects.service.ts
+++ src/app/shared/services/projects.service.ts
@@ -24,17 +24,17 @@ export class ProjectsService {
 const dummyProjects: ProjectItem[] = [
     {
       id: '1',
-      title: 'HABSAT HABSAT HABSAT',
+      title: 'HABSAT',
       url: 'https://futurelab.pk.edu.pl/wp-content/uploads/sites/38/2023/07/habsat-1536x864-1-768x432.jpg'
     },
     {
       id: '2',
-      title: 'RAKIETA RAKIETA',
+      title: 'RAKIETA',
       url: 'https://mrzigod.pl/wp-content/uploads/2022/04/90a-rakieta-760x380.jpg'
     },
     {
       id: '3',
-      title: 'GAME GAME GAME',
+      title: 'GAME',
       url: 'https://pliki.ppe.pl/storage/df96116fd582e9edec4b/df96116fd582e9edec4b.jpg'
     },
 ];
