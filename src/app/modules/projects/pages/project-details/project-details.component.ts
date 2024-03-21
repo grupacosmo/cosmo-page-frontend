e39afc37b8eaa5b46273336649c532e55899c85d
tnt120 <apaj04@gmail.com>
2024-03-21 02:25:59 +0100

    [feat] implemented fully responsive project details page

--- src/app/modules/projects/pages/project-details/project-details.component.ts
+++ src/app/modules/projects/pages/project-details/project-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ProjectDetailsItem } from 'src/app/shared/models/project-details-item';
+import { ProjectsService } from 'src/app/shared/services/projects.service';
 
 @Component({
   selector: 'app-project-details',
@@ -10,12 +12,34 @@ export class ProjectDetailsComponent implements OnInit {
 
   projectId: string | null = null;
 
+  projectDetails: ProjectDetailsItem | null = null;
+
+  teams = '';
+
+  protected text = {
+    backToProjects: 'Wszystkie projety',
+    description: 'Opis projektu',
+    experiment: 'Eksperyment',
+    team: 'Zespół'
+  }
+
   private readonly route = inject(ActivatedRoute);
+
+  private readonly projectsService = inject(ProjectsService);
 
   ngOnInit(): void {
     this.route.paramMap.subscribe((params) => {
       this.projectId = params.get('id');
     })
     
+    this.projectsService.getProject(this.projectId as string).subscribe((resp) => {
+      this.projectDetails = resp;
+
+      this.projectDetails.teams.map((item) => {
+        this.teams += item.name + ', ';
+      })
+
+      this.teams = this.teams.slice(0, -2);
+    })
   }
 }
--- src/app/shared/services/projects.service.ts
+++ src/app/shared/services/projects.service.ts
@@ -4,6 +4,7 @@ import { Observable, of } from 'rxjs';
 import { projectCardInfo } from 'src/app/modules/projects/models/project-card-info.model';
 import { ProjectItem } from '../models/project-item.model';
 import { ProjectsCardDetails } from '../models/projects-card-details.model';
+import { ProjectDetailsItem } from '../models/project-details-item';
 
 
 @Injectable({
@@ -20,6 +21,10 @@ export class ProjectsService {
 
   getProjectsCardDetails(): Observable<ProjectsCardDetails> {
     return of (dummyCardProjects);
+  }
+
+  getProject(id: string): Observable<ProjectDetailsItem> {
+    return of (dummyProjectDetails[0]);
   }
 }
 
@@ -87,3 +92,32 @@ const dummyCardProjects: ProjectsCardDetails = {
     },
   ]
 }
+
+const dummyProjectDetails: ProjectDetailsItem[] = [
+  {
+    id: '1',
+    title: 'HABSat',
+    shortDescrpiton: 'Projekt sondy stratosferycznej',
+    teams: [
+      {
+        name: 'OBC',
+        members: ['Maciej Rybiński', 'Darek Surdel', 'Jakub Kubica', 'Kamil Dziedzic', 'inż. Julia Słowikowska', 'Piotr Skała', 'inż. Wiktor Więcław']
+      },
+      {
+        name: 'WebDev',
+        members: ['Mateusz Jamróz', 'Artur Pajor', 'Adrian Cygan', 'Michał Piotrkowski']
+      }
+    ],
+    date: '02.05.2023',
+    teamPhoto: 'https://habsat.pl/static/media/team.a2ec47ec.jpg',
+    status: 'Aktywny',
+    site: 'habsat.pl',
+    siteUrl: 'https://habsat.pl/',
+    mainPhoto: 'https://futurelab.pk.edu.pl/wp-content/uploads/sites/38/2023/07/habsat-1536x864-1-768x432.jpg',
+    mainPhotoDescription: 'Zdjęcie wykonane z sondy HABsat, 02.05.2012',
+    description: 'HABSat jest urządzeniem pomiarowym do przeprowadzania eksperymentów w stratosferze na wysokości, ok. 30 km nad ziemią. Stanowi platformę eksperymentalną do bezpiecznego wysyłania w stratosferę projektów badawczych, które wymagają warunków zbliżonych do tych panujących w przestrzeni kosmicznej. Rozbudowa platformy HABSat pozwoli na zbieranie doświadczenia niezbędnego do budowy bardziej skomplikowanych konstrukcji typu CubeSat.',
+    experimentDescription: 'Detektor został opracowany przez Katarzynę Smelcerz w ramach badań do rozprawy doktorskiej: “Rozwój systemu detekcji promieniowania kosmicznego, oparty o zespół rozproszonych detektorów scyntylacyjnych” pod opieką prof. dr hab. Jerzy W. Mietelskiego. Zasada działania opiera się na wykrywaniu fotonów, które powstają przy uderzaniu cząstek kosmicznych w scyntylator (w tym wypadku kwadratowa płytka ze specjalnego tworzywa o grubości ok. 10 mm). Do środka scyntylator a przylega fotopowielacz, który wzmacnia fotony powstające przy uderzeniach cząstek kosmicznych i zamienia je na impulsy elektryczne. Impulsy są podawane na wejście wzmacniacza, a następnie są mierzone przez mikrokontroler. Mikrokontroler współpracuje z modułem GPS, z którego odczytuje czas i trzy współrzędne (szerokość i długość geograficzną oraz wysokość nad poziomem morza). Każde zdarzenie jest zapisywane do zewnętrznej pamięci Flash. Rejestrowana jest amplituda, czas zdarzenia, oraz trzy współrzędne. Detektor zostanie wysłany w górne warstwy atmosfery przy pomocy balonu. Celem tego eksperymentu jest zbadanie ilości cząstek kosmicznych jako funkcji wysokości. Uważa się, że atmosfera przechwytuje cząstki (tłumi promieniowanie kosmiczne), zatem wysoko ilość detekcji powinna być większa. Kolejne zagadnienie, które jest badane, to jak przebiega tłumienie cząstek wysokoenergetycznych w funkcji wysokości w porównaniu z tłumieniem cząstek niskoenergetycznych.',
+    photos: ['https://habsat.pl/static/media/eksperyment.3258c20e.jpg'],
+    photosDescriptions: ['Detektor']
+  }
+]
