bc4eca28e6ce1373a5e655292feca4372f4d08e4
Adrian Cygan <101710079+zaba51@users.noreply.github.com>
2024-05-11 14:45:44 +0200

    Achievments (#7)

---
+++ src/app/modules/achievments/components/achievment-details-dialog/achievment-details-dialog.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AchievmentDetailsDialogComponent } from './achievment-details-dialog.component';
+
+describe('AchievmentDetailsDialogComponent', () => {
+  let component: AchievmentDetailsDialogComponent;
+  let fixture: ComponentFixture<AchievmentDetailsDialogComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AchievmentDetailsDialogComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(AchievmentDetailsDialogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/shared/models/achievments.ts
@@ -0,0 +1,7 @@
+export interface AchievmentItem {
+    id: number,
+    date: string,
+    icon: string,
+    title: string,
+    description: string,
+}
