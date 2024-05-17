498f264dd6c4564ac51986f7a2182208a06388fb
Mateusz Jamróz <jamr.mat@gmail.com>
2024-05-17 23:11:39 +0200

    slight content adjustments

--- src/app/modules/home/components/about-us/about-us.component.ts
+++ src/app/modules/home/components/about-us/about-us.component.ts
@@ -1,19 +1,17 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-about-us',
-  templateUrl: './about-us.component.html',
-  styleUrl: './about-us.component.scss'
+    selector: 'app-about-us',
+    templateUrl: './about-us.component.html',
+    styleUrl: './about-us.component.scss'
 })
 export class AboutUsComponent {
-  protected text = {
-    title: 'O nas',
-    about: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin urna leo, vestibulum malesuada justo sed, 
-            rhoncus cursus dui. In non luctus ante. Aliquam vitae arcu euismod, volutpat erat nec,
-            pulvinar ligula. Fusce purus elit, varius eu neque quis, eleifend porta leo.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin urna leo, vestibulum malesuada justo sed, 
-            rhoncus cursus dui. In non luctus ante. Aliquam vitae arcu euismod, volutpat erat nec,
-            pulvinar ligula. Fusce purus elit, varius eu neque quis, eleifend porta leo.`,
-    meetOurTeam: 'Poznaj nasz zespół'
-  }
+    protected text = {
+        title: 'O nas',
+        about: `Jesteśmy grupą studentów, z różnych wydziałów, z różnymi umiejętnościami, których łączy jedno –
+zainteresowanie kosmosem. Działamy na Politechnice Krakowskiej od 2019 roku, mamy za sobą
+zrealizowane projekty i plany na realizację kolejnych. Obecnie zajmujemy się budową pierwszej
+rakiety sondującej, drona szybowcowego oraz projektujemy komputerową grę edukacyjną.`,
+        meetOurTeam: 'Poznaj nasz zespół'
+    }
 }
--- src/app/modules/home/components/join-us/join-us.component.ts
+++ src/app/modules/home/components/join-us/join-us.component.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-join-us',
-  templateUrl: './join-us.component.html',
-  styleUrl: './join-us.component.scss'
+    selector: 'app-join-us',
+    templateUrl: './join-us.component.html',
+    styleUrl: './join-us.component.scss'
 })
 export class JoinUsComponent {
-  protected text = {
-    joinCosmo: 'DOŁĄCZ DO COSMO PK',
-    summary: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin urna leo, vestibulum malesuada justo sed.',
-    learnMore: 'Dowiedz się więcej'
-  }
+    protected text = {
+        joinCosmo: 'DOŁĄCZ DO COSMO PK',
+        summary: 'Jeśli chcesz się przyłączyć do jednego z naszych projektów lub masz własny pomysł, wyślij nam swoje zgłoszenie – odezwiemy się do Ciebie',
+        learnMore: 'Dowiedz się więcej'
+    }
 }
