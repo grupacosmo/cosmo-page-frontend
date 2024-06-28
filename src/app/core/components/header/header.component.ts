399891ea00f79c55fcbc1032b1085be18be1788c
tnt120 <apaj04@gmail.com>
2024-06-28 15:45:25 +0200

    [feat] animation in header & working hamburger menu on mobile

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss'],
+  animations: [
+    trigger('slideInOut', [
+      state('in', style({
+        transform: 'translateY(0)'
+      })),
+      state('out', style({
+        transform: 'translateY(-100%)'
+      })),
+      transition('out => in', [
+        animate('0.5s ease-out')
+      ]),
+      transition('in => out', [
+        animate('0.5s ease-in')
+      ])
+    ])
+  ]
 })
 export class HeaderComponent {
+  menuOpen = false;
 
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  get menuState() {
+    return this.menuOpen ? 'in' : 'out';
+  }
 }
