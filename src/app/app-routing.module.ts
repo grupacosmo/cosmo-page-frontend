6a8ebf07c9f30632df4cbbaef16669e63ac165d8
tnt120 <apaj04@gmail.com>
2024-11-12 21:36:47 +0100

    [refactor] delete unused button & hide news and achievments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,11 +8,11 @@ const routes: Routes = [
     loadChildren: () =>
       import('./modules/home/home.module').then((m) => m.HomeModule),
   },
-  {
-    path: 'news',
-    loadChildren: () =>
-      import('./modules/news/news.module').then((m) => m.NewsModule),
-  },
+  // {
+  //   path: 'news',
+  //   loadChildren: () =>
+  //     import('./modules/news/news.module').then((m) => m.NewsModule),
+  // },
   // {
   //   path: 'team',
   //   loadChildren: () =>
@@ -23,11 +23,11 @@ const routes: Routes = [
     loadChildren: () =>
       import('./modules/projects/projects.module').then((m) => m.ProjectsModule),
   },
-  {
-    path: 'achievments',
-    loadChildren: () =>
-      import('./modules/achievments/achievments.module').then((m) => m.AchievmentsModule),
-  },
+  // {
+  //   path: 'achievments',
+  //   loadChildren: () =>
+  //     import('./modules/achievments/achievments.module').then((m) => m.AchievmentsModule),
+  // },
   {
     path: 'admin',
     loadChildren: () =>
