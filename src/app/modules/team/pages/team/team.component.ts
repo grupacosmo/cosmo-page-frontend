313116fc3b5759aea4de19d94f0fb97bef942477
acygan <acygan51@gmail.com>
2025-11-09 16:28:46 +0100

    team section

--- src/app/modules/team/pages/team/team.component.ts
+++ src/app/modules/team/pages/team/team.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component, signal } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { teams } from 'src/assets/data/team';
 @Component({
     selector: 'app-team',
     templateUrl: './team.component.html',
-    styleUrl: './team.component.scss'
+    styleUrl: './team.component.scss',
+    imports: [NgClass]
 })
 export class TeamComponent {
+    readonly activeTeam = signal(""); 
 
+    readonly teamMembers = teams;
+
+    scrollToTeam(teamId: string) {
+        const el = document.getElementById(teamId);
+        if (el) el.scrollIntoView({ behavior: 'smooth' });
+    }
 }
---
+++ src/assets/data/team.ts
@@ -0,0 +1,288 @@
+export const teams = [
+    {
+        team: 'Logistyka',
+        members: [
+            {
+                name: 'Jakub Kubica',
+                roles: ['Przewodniczący koła naukowego Cosmo PK'],
+                image: 'assets/images/members/jakub_kubica.png',
+                description: 'Student 1 roku II stopnia Informatyki - Cyberbezpieczeństwo. Z kołem związany ponad 2 lata - początkowo w projekcie HabSat, gdzie miałem pierwszą styczność z programowaniem mikrokontrolerów. Stopniowo zacząłem bardziej się angażować organizacyjnie (organizacja wyjazdów, rekrutacja członków), do momenty kiedy przejąłem obowiązki ówczesnego przewodniczącego. Obecnie dbam o rozwój całego pod kątem merytorycznym i organizacyjnym, a także odpowiadam za współpracę z jednostkami naukowymi i firmami.'
+            },
+            {
+                name: 'Patryk Paluch',
+                roles: ['Wsparcie techniczne i organizacyjne'],
+                image: 'assets/images/members/patryk_paluch.jpg',
+                description: 'Brałem udział w wielu już zakończonych projektach koła i w wielu aktualnych się aktywnie udzielam. Zawsze śpieszę z pomocą i chętnie dzielę się doświadczeniem i wiedzą. Najaktywniej znajdziecie mnie w GameDevie, Rakiecie i Logistyce. Mimo skończenia już studiów informatycznych to nadal jestem aktywnym członkiem koła od 2023-04'
+            },
+            {
+                name: 'Karol Kusiak',
+                roles: ['Wsparcie organizacyjne'],
+                image: 'assets/images/members/male.png',
+                description: 'Student Informatyki w inżynierii komputerowej, działający na kole od 8 listopada 2024 roku. Moim zadaniem jest tworzenie współprac z firmami oraz organizacja wyjść na wydarzenia związanymi z podniesieniem umiejętności koła. Moimi hobby to druk 3D, elektronika, historia oraz geografia.'
+            },
+            {
+                name: 'Wojciech Majcherczyk',
+                roles: ['Wsparcie organizacyjne'],
+                image: 'assets/images/members/male.png',
+                description: 'Student 2 roku na kierunku pojazdy samochodowe na wydziale mechanicznym. Działający na kole od kwietnia 2025 roku. Moje hobby to wszelkiej maści symulatory oraz wędrówki po górach.'
+            }
+        ]
+    },
+    {
+        team: 'Webdev',
+        members: [
+            {
+                name: 'Michał Piotrkowski',
+                roles: ['Koordynator', 'Programista Frontend'],
+                image: 'assets/images/members/male.png',
+                description: ''
+            },
+            {
+                name: 'Adrian Cygan',
+                roles: ['Programista Frontend', 'UI Designer'],
+                image: 'assets/images/members/adrian_cygan.jpg',
+                description: 'Student Data Science na WIiM. Członek koła od 2022r. Uczestniczyłem w projektach HABsat, COSMO Quiz, jestem głównym projektantem i developerem nowej strony koła. Interesuję się muzyką, astronomią i kinem. Prywatnie gitarzysta/perkusista.'
+            },
+            {
+                name: 'Zuzia Zawartka',
+                roles: ['Programista Backend'],
+                image: 'assets/images/members/female.png',
+                description: ''
+            },
+            {
+                name: 'Yaroslav Svitlytskyi',
+                roles: ['Programista Backend'],
+                image: 'assets/images/members/male.png',
+                description: ''
+            },
+            {
+                name: 'Artur Pajor',
+                roles: ['Programista Frontend'],
+                image: 'assets/images/members/male.png',
+                description: ''
+            },
+            {
+                name: 'Mateusz Miś',
+                roles: ['Stażysta'],
+                image: 'assets/images/members/male.png',
+                description: 'Student 2 roku na IwIK-u, w CosmoPK od października 2025 roku. Wzięty pod skrzydła starszych, aby rozwinąć swoje doświadczenie w WebDevie. Dodatkowo członek sekcji szachowej PK. Interesuję się grą na basie oraz historią z okresu I i II WW.'
+            }
+        ]
+    },
+    {
+        team: 'Rakieta',
+        members: [
+            {
+                name: 'Bartosz Słowik',
+                roles: ['Mentor', 'Koordynator'],
+                image: 'assets/images/members/bartosz_slowik.jpg',
+                description: 'W kole zajmowałem się koordynowaniem zespołu CanSat, a później Rakiety. Skupiam się nad projektem i programowaniem oprogramowania komputerów pokładowych, oraz zarządzaniu projektem na testach silnika lub startach rakiety. W CosmoPK od maja 2022.'
+            },
+            {
+                name: 'Patryk Paluch',
+                roles: ['Mentor'],
+                image: 'assets/images/members/patryk_paluch.jpg',
+                description: 'Brałem udział w wielu już zakończonych projektach koła i w wielu aktualnych się aktywnie udzielam. Zawsze śpieszę z pomocą i chętnie dzielę się doświadczeniem i wiedzą. Najaktywniej znajdziecie mnie w GameDevie, Rakiecie i Logistyce. Mimo skończenia już studiów informatycznych to nadal jestem aktywnym członkiem koła od 2023-04'
+            },
+            {
+                name: 'Jakub Plicner',
+                roles: ['Inżynier Rakiety', 'Silnika i paliwa rakietowego'],
+                image: 'assets/images/members/male.png',
+                description: 'Uczeń 1LO w Krakowie, w CosmoPK od października 2023. Po dołączeniu do koła tworzyłem rakietę Zorza 1 i platformę testową oraz pomagałem w rozwoju naszego silnika rakietowego i paliwa rakietowego razem z Pawłem Niedzielą. Prywatnie żeglarz oraz członek Szybowcowej Kadry Narodowej Juniorów.'
+            },
+            {
+                name: 'Paweł Niedziela',
+                roles: ['Inżynier Rakiety', 'Silnika i paliwa rakietowego'],
+                image: 'assets/images/members/male.png',
+                description: 'Uczeń Technikum New Technology na profilu technik programista, w CosmoPK od października 2022. Po moim pierwszym projekcie którym była rakieta Swara 4 zająłem się produkcją i rozwojem naszego silnika i paliwa rakietowego razem z Jakubem Plicnerem.'
+            },
+            {
+                name: 'Piotr Zyguła',
+                roles: ['Inżynier'],
+                image: 'assets/images/members/male.png',
+                description: 'Student Infotroniki, działający w kole od października 2024r. Przygodę w Cosmo rozpocząłem od zabezpieczania testów rakiety. Później zajmowałem się już postprocessingiem elementów i testowaniem zapalników. Uwielbiam majsterkować i zawsze wymyślam kreatywne rozwiązania problemów technicznych.'
+            },
+            {
+                name: 'Oliwier Sujka',
+                roles: ['Inżynier'],
+                image: 'assets/images/members/male.png',
+                description: 'Uczeń Technikum New Technology na profilu technik robotyk, w cosmo od 3 października 2025. Do tej pory miałem okazję zmodyfikować platformę testową rakiet. Prywatnie zajmuje się budowaniem robotów. Lubię ludzi, kosmos i dinozaury.'
+            },
+            {
+                name: 'Władysław Chornak',
+                roles: ['Inżynier'],
+                image: 'assets/images/members/wladysław_chornak.png',
+                description: 'Uczeń Technikum New Technology na profilu technik robotyk, w Cosmo od 3 października 2025. Moim pierwszym zadaniem było zmodyfikowania platformy testowej rakiety. Pasjonuje się wszystkim co związane z robotyką, fizyką oraz mechaniką.'
+            },
+            {
+                name: 'Tomasz Klimek',
+                roles: ['Inżynier oprogramowania rakiety'],
+                image: 'assets/images/members/male.png',
+                description: 'Student Informatyki, w Cosmo od listopada 2024. Uczestniczyłem w programowaniu komputera pokładowego rakiety, od października 2025 jestem głównym programistą nowej wersji oprogramowania komputera pokładowego.'
+            },
+            {
+                name: 'Karol Kusiak',
+                roles: ['Inżynier', 'tester oprogramowania symulacyjnego', 'programista'],
+                image: 'assets/images/members/male.png',
+                description: 'Student Informatyki w inżynierii komputerowej, działający na kole od 8 listopada 2024 roku. Moim zadaniem jest tworzenie symulacji rakiet oraz stworzenie stacji pogodowej. Moimi hobby to druk 3D, elektronika, historia oraz geografia.'
+            },
+            {
+                name: 'Michał Mączka',
+                roles: ['Koordynator', 'sprawy elektroniki i komunikacji radiowej'],
+                image: 'assets/images/members/male.png',
+                description: 'Student Elektrotechniki, w cosmo od października 2024, Pasjonat FPV, fotografii, elektroniki i mikrokontrolerów. Projektuje i drukuje 3d, projektuje pcb, zaczynam ambitne projekty po czym nigdy ich nie kończę.'
+            }
+        ]
+    },
+    {
+        team: 'Dron',
+        members: [
+            {
+                name: 'Patryk Kusper',
+                roles: ['Koordynator', 'Pilot BSP'],
+                image: 'assets/images/members/patryk_kusper.jpg',
+                description: 'Interesuje się budową i pilotowaniem Bezzałogowych Statków Powietrznych, szczególnie dronów FPV, oraz tworzeniem aplikacji mobilnych. Jestem koordynatorem zespołu budującego drona i zajmuje się sekcją elektroniki i sterowania.'
+            },
+            {
+                name: 'Kinga Kaim',
+                roles: ['Projektantka 3D'],
+                image: 'assets/images/members/female.png',
+                description: 'Pasjonuje mnie łączenie umiejętności architektonicznych z sekcją kosmiczną, stąd modelowanie części drona, a następnie obrabianie ich i składanie po wydrukowaniu na drukarce 3D. Kręcą mnie projekty baz czy habitatów na innych planetach, co wdrażam w pracę inżynierską. Poza tym fascynuje mnie badanie zachowań astronautów, czego mogłam doświadczyć podczas wyjazdu na symulację misji kosmicznej.'
+            },
+            {
+                name: 'Mateusz Bielat',
+                roles: ['Projektant 3D'],
+                image: 'assets/images/members/male.png',
+                description: ''
+            }
+        ]
+    },
+    {
+        team: 'GameDev',
+        members: [
+            {
+                name: 'Adam Kiraly',
+                roles: ['Koordynator', 'Designer', 'Programista'],
+                image: 'assets/images/members/male.png',
+                description: 'Jestem studentem informatyki, do koła dołączyłem w Październiku 2023. Zaczynając jako programista w zespole Gamedev, z czasem zostałem koordynatorem zespołu. Interesuję się wszelkiego rodzaju informatyką i programowaniem, game designem, a także astronomią, elektroniką oraz fizyką 🪐'
+            },
+            {
+                name: 'Patryk Paluch',
+                roles: ['Koordynator', 'Programista'],
+                image: 'assets/images/members/patryk_paluch.jpg',
+                description: 'Brałem udział w wielu już zakończonych projektach koła i w wielu aktualnych się aktywnie udzielam. Zawsze śpieszę z pomocą i chętnie dzielę się doświadczeniem i wiedzą. Najaktywniej znajdziecie mnie w GameDevie, Rakiecie i Logistyce. Mimo skończenia już studiów informatycznych to nadal jestem aktywnym członkiem koła od 2023-04'
+            },
+            {
+                name: 'Stanisław Leszczyński',
+                roles: ['Programista', 'czasem inne'],
+                image: 'assets/images/members/stanislaw_leszczynski.png',
+                description: 'Pasjonat gamedevu i wszystkiego co z nim związane od maleńkości. Znam się na prawie wszystkim związanym z gamedevem, mniej lub więcej, zwykle mniej, ale fajnie popróbować. Członek Cosmo od grudnia 2024.'
+            },
+            {
+                name: 'Kacper Lewandowski',
+                roles: ['Programista'],
+                image: 'assets/images/members/male.png',
+                description: 'Student, członek Cosmo od stycznia 2024. Lubię grać w gry to uznałem, że będę je też robił. Od tamtego momentu trochę minęło ale jednak tu jestem.'
+            },
+            {
+                name: 'Miłosz Głąbiński',
+                roles: ['Programista'],
+                image: 'assets/images/members/milosz_glabinski.jpg',
+                description: 'Student informatyki Uniwersytetu Ekonomicznego w Krakowie, dołączyłem do koła chcąc dowiedzieć się więcej o gamedevie. Interesuję się programowaniem oraz informatyką. W wolnym czasie grywam w tenisa i szachy, lubię też czytać książki Sci-Fi oraz reportaże. W CosmoPK od listopada 2024.'
+            }
+        ]
+    },
+    {
+        team: 'HabSat',
+        members: [
+            {
+                name: 'Grzegorz Wiktor',
+                roles: ['Koordynator Techniczny', 'Hardware'],
+                image: 'assets/images/members/grzegorz_wiktor.png',
+                description: 'Uczeń Zespołu Szkół Energetycznych w Krakowie na kierunku technik-elektronik. Członek zwycięskiego zespołu ZSE-Space w konkursie Cansat2024-Poland. Moje hobby to elektronika, druk 3D oraz airsoft. Planuję studiować na Politechnice. Dołączyłem do COSMO PK w kwietniu 2025 roku.'
+            },
+            {
+                name: 'Karol Kusiak',
+                roles: ['Programista', 'Tester oprogramowania'],
+                image: 'assets/images/members/male.png',
+                description: 'Student Informatyki w inżynierii komputerowej, działający na kole od 8 listopada 2024 roku. Moim zadaniem jest tworzenie symulacji HabSata oraz stworzenie oprogramowania do komunikacji. Moimi hobby to druk 3D, elektronika, historia oraz geografia.'
+            },
+            {
+                name: 'Wojciech Majcherczyk',
+                roles: ['Koordynator', 'wsparcie logistyczne'],
+                image: 'assets/images/members/male.png',
+                description: 'Student 2 roku na kierunku pojazdy samochodowe na wydziale mechanicznym. Działający na kole od kwietnia 2025 roku. Moje hobby to wszelkiej maści symulatory oraz wędrówki po górach.'
+            }
+        ]
+    },
+    {
+        team: 'Robotyka',
+        members: [
+            {
+                name: 'Aleksy Biela',
+                roles: ['Koordynator'],
+                image: 'assets/images/members/aleksy_biela.jpg',
+                description: 'Aktualnie studiuję informatykę na trzecim roku na Wydziale Informatyki i Matematyki. Do koła naukowego dołączyłem w listopadzie 2023 roku. Swoją przygodę w kole rozpocząłem w zespole rakietowym, jednak obecnie rozwijam własną sekcję robotyczną. Prywatnie pasjonuję się szyciem oraz biologią. Czasami, choć nieregularnie, uczestniczę również w zajęciach z fechtunku i jazdy konno.'
+            },
+            {
+                name: 'Mateusz Bojarczuk',
+                roles: ['Symulacja i analiza danych'],
+                image: 'assets/images/members/male.png',
+                description: 'Właśnie zacząłem swój pierwszy rok na PK na kierunku Automatyka i Robotyka na wydziale mechanicznym. Po raz pierwszy w życiu uczestniczę w projekcie naukowym. Moim hobby jest matematyka i gry komputerowe. Od czasu do czasu trenuję boks.'
+            },
+            {
+                name: 'Dominik Madej',
+                roles: ['Członek zespołu'],
+                image: 'assets/images/members/male.png',
+                description: ''
+            },
+            {
+                name: 'Filip Szczepański',
+                roles: ['Członek zespołu'],
+                image: 'assets/images/members/male.png',
+                description: ''
+            },
+            {
+                name: 'Jacek Łukasik',
+                roles: ['Elektronik'],
+                image: 'assets/images/members/male.png',
+                description: 'Jestem studentem trzeciego roku Automatyki i Robotyki na Wydziale Mechanicznym. Niedawno dołączyłem do koła naukowego, aby rozwijać swoje umiejętności techniczne, wykorzystać wiedzę zdobytą w trakcie studiów oraz podjąć się nowych wyzwań.'
+            },
+            {
+                name: 'Paweł Zając',
+                roles: ['Członek zespołu'],
+                image: 'assets/images/members/male.png',
+                description: ''
+            },
+            {
+                name: 'Maciek Skrowron',
+                roles: ['Członek zespołu'],
+                image: 'assets/images/members/male.png',
+                description: ''
+            }
+        ]
+    },
+    {
+        team: 'AI',
+        members: [
+            {
+                name: 'Kacper Figura',
+                roles: ['Analityk Danych', 'Inżynier ds. Modelowania'],
+                image: 'assets/images/members/male.png',
+                description: 'Jestem aktywnym studentem specjalizującym się w analizie danych i budowie modeli statystycznych. Interesuję się data engineeringiem i tworzeniem pipeline’ów ML. Rozwijam się algorytmicznie, doskonaląc rozwiązywanie problemów programistycznych. Pasjonuję się futbolem i analizą sportowych danych.'
+            },
+            {
+                name: 'Jan Świdziński',
+                roles: ['Inżynier Sztucznej Inteligencji i Środowiska Projektowego'],
+                image: 'assets/images/members/male.png',
+                description: 'Interesuję się programowaniem, symulacjami inspirowanymi naturą i sztuczną inteligencją. Fascynuje mnie, jak proste zasady tworzą złożone zachowania. W kole zajmuję się AI i symulacjami. Po godzinach gram w ci piszę do szuflady. Jestem w kole od grudnia 2024 r.'
+            },
+            {
+                name: 'Patryk Janik',
+                roles: ['Członek Zespołu'],
+                image: 'assets/images/members/male.png',
+                description: 'Interesuje się GameDevem i szerokopojętymi zgadnieniami związanymi z AI. Choć wciąż się uczę a progress idzie powoli, w kole zajmujemy się detekcją obiektów na dronie. Prywatnie gram na gitarze a od niedawna uczęszczam na boks.  W kole jestem od grudnia 2024r.'
+            },
+        ]
+    },
+];
