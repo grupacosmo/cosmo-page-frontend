3a9e0010d3c0f15b01bf91e51437cd62284c4b5a
Adrian Cygan <101710079+zaba51@users.noreply.github.com>
2026-03-01 18:49:33 +0100

    Feature/connect backend (#57)
    * show news on production
    
    * update build to include api_key
    
    * fix env name
    
    * fix system env & add url

--- src/custom-webpack.config.ts
+++ src/custom-webpack.config.ts
@@ -1,6 +1,15 @@
 import { EnvironmentPlugin } from 'webpack';
-
 const Dotenv = require('dotenv-webpack');
+// const webpack = require('webpack');
+
 module.exports = {
-  plugins: [new Dotenv()],
+  plugins: [
+    new Dotenv({
+      systemvars: true,
+      silent: true
+    }),
+    // new webpack.DefinePlugin({
+    //   'process.env': JSON.stringify(process.env)
+    // })
+  ],
 };
